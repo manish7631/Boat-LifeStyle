4101e9e0c3abe6c63c693b1683db908df2aec768
manishfw13 <manish_fw13_347@masai.school>
2022-08-17 13:24:53 +0530

    slider add

--- src/components/Home/Slider/Slider.jsx
+++ src/components/Home/Slider/Slider.jsx
@@ -33,11 +33,20 @@ export default class PauseOnHover extends Component {
             afterChange: current => this.setState({ activeSlide2: current })
         };
         return (
-            <Box style={{
-                width: "100%",
-                height: "420px",
-                // border:"1px solid red"
-            }}>
+            <Box
+                sx={{
+                    width: '100%',
+                    height: {
+                        xs: 300,
+                        sm: 400,
+                        md: 420,
+                        lg: 420,
+                        xl: 420
+
+                    },
+
+                }}
+            >
                 <Button onClick={this.previous} style={
                     {
                         border: "1px solid red",
@@ -85,26 +94,54 @@ export default class PauseOnHover extends Component {
 
                         <img style={{
                             width: "100%",
-                            height: "420px",
+                            height: {
+                                xs: 300,
+                                sm: 400,
+                                md: 420,
+                                lg: 420,
+                                xl: 420
+
+                            },
                         }} src="https://cdn.shopify.com/s/files/1/0057/8938/4802/files/Sail_with_boAt_WEB_1400x.jpg?v=1652272257" alt="" />
 
                     </Card>
                     <Card key={2}>
                         <img style={{
                             width: "100%",
-                            height: "420px",
+                            height: {
+                                xs: 300,
+                                sm: 400,
+                                md: 420,
+                                lg: 420,
+                                xl: 420
+
+                            },
                         }} src="https://cdn.shopify.com/s/files/1/0057/8938/4802/files/STONE-1450WEB_48633fa7-b4fb-46d3-b1b2-f90a8bff6df4_1400x.jpg?v=1652272312" alt="" />
                     </Card>
                     <Card key={3}>
                         <img style={{
                             width: "100%",
-                            height: "420px",
+                            height: {
+                                xs: 300,
+                                sm: 400,
+                                md: 420,
+                                lg: 420,
+                                xl: 420
+
+                            },
                         }} src="https://cdn.shopify.com/s/files/1/0057/8938/4802/files/Watch_xtend_wbann_1_1400x.jpg?v=1652272357" alt="" />
                     </Card>
                     <Card key={4}>
                         <img style={{
                             width: "100%",
-                            height: "420px",
+                            height: {
+                                xs: 300,
+                                sm: 400,
+                                md: 420,
+                                lg: 420,
+                                xl: 420
+
+                            },
                         }} src="https://cdn.shopify.com/s/files/1/0057/8938/4802/files/WhatsApp_Image_2022-04-29_at_11.32.28_AM_1400x.jpg?v=1652272398" alt="" />
                     </Card>
 
